ade2573bebe031eeecc171dd0fcf9344398baece
TzvetanIG <cecoig@mail.bg>
2015-01-06 09:20:15 +0200

    rename options-controller.js to main-controller.js

---
+++ js/controllers/main-controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+adsApp.controller('MainController', function ($scope, pageOptions) {
+    $scope.pageTitle =  'Ads - Home';
+    $scope.navigationTemplate = 'templates/includes/guest-menu.html';
+});
--- js/controllers/options-controller.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var adsApp = adsApp || {};
-
-adsApp.controller('optionsController', function ($scope, pageOptions) {
-    $scope.header = pageOptions.getPageTitle();
-    $scope.template = pageOptions.getMainMenu();
-});
