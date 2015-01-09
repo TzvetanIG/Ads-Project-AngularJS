530230442fe2ee6e85b2f0a71cb824a58293b943
TzvetanIG <cecoig@mail.bg>
2015-01-09 18:01:17 +0200

    Updated user profile

--- js/controllers/edit-profile-controller.js
+++ js/controllers/edit-profile-controller.js
@@ -1,8 +1,21 @@
 'use strict';
 
-adsApp.controller('EditProfileController', ['$scope', 'townsData', 'pageOptions',
-    function ($scope, townsData, pageOptions) {
+adsApp.controller('EditProfileController', ['$scope', 'townsData', 'pageOptions', 'userData', 'messageData',
+    function ($scope, townsData, pageOptions, userData, messageData) {
         pageOptions.setPageTitle('Ad - Edit Ad');
         $scope.towns = townsData.getTowns();
+
+        $scope.user =  userData.getUserProfile();
+
+        $scope.updateUser = function (user) {
+            userData.editUserProfile(user,
+                function (data) {
+                    messageData.sentInfoMessage('Success update');
+                },
+                function (error) {
+                    messageData.sentErrorMessage('Update failed', error)
+                }
+            );
+        }
     }]
 );
--- js/services/user-data.js
+++ js/services/user-data.js
@@ -1,21 +1,63 @@
 'use strict';
 
-adsApp.factory('userData', ['$http', 'basicUrl', function ($http, basicUrl) {
-    var url = basicUrl + 'user/login';
+adsApp.factory('userData', ['$http', '$resource', 'basicUrl', function ($http, $resource, basicUrl) {
+
+    function getUserProfile(success, error) {
+        var user = getCurrentUser();
+        var userProfileResource = $resource(
+            basicUrl + 'user/profile',
+            null,
+            {
+                'getUserProfile': {method: 'GET', headers: {Authorization: 'Bearer ' + user.access_token}}
+            }
+        );
+
+        return userProfileResource.getUserProfile(success, error);
+    }
+
+    function editUserProfile(userData, success, error) {
+        var user = getCurrentUser();
+        var userProfileResource = $resource(
+            basicUrl + 'user/profile',
+            null,
+            {
+                'getUserProfile': {
+                    method: 'PUT',
+                    headers: {Authorization: 'Bearer ' + user.access_token},
+                    data: userData
+                }
+            }
+        );
+
+        return userProfileResource.getUserProfile(userData, success, error);
+    }
+
+    function changePassword(success, error) {
+        var user = getCurrentUser();
+        var userProfileResource = $resource(
+            basicUrl + 'user/changepassword',
+            null,
+            {
+                'changePassword': {method: 'PUT', headers: {Authorization: 'Bearer ' + user.access_token}}
+            }
+        );
+
+        return userProfileResource.getUserProfile(success, error);
+    }
 
     function login(user, success, error) {
-            var request = {
-                method: 'POST',
-                url: basicUrl + 'user/login',
-                data: user
-            };
+        var request = {
+            method: 'POST',
+            url: basicUrl + 'user/login',
+            data: user
+        };
 
-            $http(request)
-                .success(function(data) {
-                    sessionStorage['currentUser'] = JSON.stringify(data);
-                    success(data);
-                })
-                .error(error);
+        $http(request)
+            .success(function (data) {
+                sessionStorage['currentUser'] = JSON.stringify(data);
+                success(data);
+            })
+            .error(error);
     }
 
     function register(user, success, error) {
@@ -26,27 +68,30 @@ adsApp.factory('userData', ['$http', 'basicUrl', function ($http, basicUrl) {
         };
 
         $http(request)
-            .success(function(data) {
+            .success(function (data) {
                 success(data);
             })
             .error(error);
     }
 
-    function getCurrentUser(){
+    function getCurrentUser() {
         var userObject = sessionStorage['currentUser'];
         if (userObject) {
             return JSON.parse(sessionStorage['currentUser']);
         }
     }
 
-    function logout(){
-            delete sessionStorage['currentUser'];
+    function logout() {
+        delete sessionStorage['currentUser'];
     }
 
     return {
         login: login,
         getCurrentUser: getCurrentUser,
         logout: logout,
-        register: register
+        register: register,
+        getUserProfile: getUserProfile,
+        changePassword: changePassword,
+        editUserProfile: editUserProfile
     };
 }]);
