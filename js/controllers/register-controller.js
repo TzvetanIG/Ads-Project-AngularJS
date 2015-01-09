e43b494934da8fa9dfadd542d74b12a3eb53122f
TzvetanIG <cecoig@mail.bg>
2015-01-09 09:10:42 +0200

    Added towns select in registration form

--- js/controllers/register-controller.js
+++ js/controllers/register-controller.js
@@ -1,7 +1,10 @@
 'use strict';
 
-adsApp.controller('RegisterController', function ($scope, $rootScope, pageOptions, userData, messageData) {
+adsApp.controller('RegisterController', function ($scope, $rootScope, pageOptions,
+                                                  userData, messageData, townsData) {
     pageOptions.setPageTitle('Ads - Registration');
+
+    $scope.towns = townsData.getTowns();
 
     $scope.login = function (user) {
         if (user && user.username && user.password) {
